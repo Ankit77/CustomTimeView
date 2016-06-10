b42465fb0450a4b8a5e20c19620ef2ef743e8c52
nrupparikh <webmaster@indianic.com>
2016-06-10 17:58:56 +0530

    toast msg change

--- app/src/main/java/com/example/customtimeview/MainActivity.java
+++ app/src/main/java/com/example/customtimeview/MainActivity.java
@@ -29,6 +29,6 @@ public class MainActivity extends AppCompatActivity implements CustomDateandTime
 
     @Override
     public void onSelect() {
-        Toast.makeText(getApplicationContext(), "sdk", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "sdk123", Toast.LENGTH_LONG).show();
     }
 }
